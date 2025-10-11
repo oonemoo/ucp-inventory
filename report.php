d2539b9f92e720a1c24551b73787d0c347a653bd
oone <bariquitsheilaaa@gmail.com>
2025-10-11 14:48:32 +0800

    added code to report.php and supplier.php

--- report.php
+++ report.php
@@ -2,9 +2,73 @@
 
     <!-- CSS style-->
     <style>
-
+        .maincontent {
+            display: flex;
+            flex-direction: column;
+            padding: 20px;
+        }
+        .box-container {
+            box-shadow: 0 0 15px 2px whitesmoke;
+            backdrop-filter: blur(12px) saturate(180%);
+            -webkit-backdrop-filter: blur(12px) saturate(180%);
+            background-color: rgba(255, 255, 255, 0.44);
+            border-radius: 12px;
+            border: 1px solid rgba(209, 213, 219, 0.3);
+            padding: 20px;
+        }
+        .grid-container {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            gap: 20px;
+        }
     </style>
 
-</body>
+    <div class="maincontent">
+        <h3>Reports</h3>
+        <div class="box-container">
+            <div class="grid-container grid-item">
+                    <header>
+                        
+                    </header>
 
+                    <section class="overview">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, eius ut at ratione aperiam qui accusamus sapiente magnam, rerum totam sunt quis non, natus tempora vel itaque voluptates nam reprehenderit.</p>
+                    </section>
+
+                    <section class="monthly-performance">
+                        <canvas id="myLineChart" width="400" height="200"></canvas>
+                        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+                        <!--Line Chart -->
+                        <script>
+                            const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+                            const data = {
+                            labels: labels,
+                            datasets: [{
+                                label: 'My First Dataset',
+                                data: [65, 59, 80, 81, 56, 55, 40],
+                                fill: false,
+                                borderColor: 'rgb(75, 192, 192)',
+                                tension: 0.1
+                            }]
+                            };
+
+                            const config = {
+                            type: 'line',
+                            data: data,
+                            };
+
+                            const ctx = document.getElementById('myLineChart').getContext('2d');
+                            const myLineChart = new Chart(ctx, config);
+                        </script>
+            
+                    </section>
+
+                    <section>
+                    
+                    </section>
+            </div>                    
+        </div>
+    </div>
+
+</body>
 </html>
--- supplier.php
+++ supplier.php
@@ -5,6 +5,48 @@
 
      </style>
 
+     <div class="maincontent">
+        <div class="table-container">
+
+        <h1>Supplier</h1>
+              <table id="example" class="table">
+                <thead>
+                    <tr>
+                        <th>SKU</th>
+                        <th>Product Name</th>
+                        <th>Description</th>
+                        <th>Category</th>
+                        <th>Quantity</th>
+                        <th>Date Received</th>
+                        <th>Supplier</th>
+                        <th>Location</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td data-search="">#245</td>
+                        <td>PC</td>
+                        <td>lorem</td>
+                        <td>Hardware</td>
+                        <td data-order="1323129600">5</td>
+                        <td data-order="145600">June 16, 1991</td>
+                        <td>ABC Supplier</td>
+                        <td>Warehouse</td>
+                        <td>
+                            <button class="edit-btn">Edit</button>
+                            <button class="delete-btn">Delete</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+    </div>
+        </div>
+     </div>
+
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.3.4/css/dataTables.dataTables.css" />
+    <script src="https://cdn.datatables.net/2.3.4/js/dataTables.js"></script>
 </body>
 
 </html>
