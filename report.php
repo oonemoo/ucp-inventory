01490d5ce59cc052d21edc6aec20b6a89e1c1196
bariquit <bariquitsheilaaa@gmail.com>
2025-10-12 18:16:44 +0800

    add some changes

--- report.php
+++ report.php
@@ -1,30 +1,6 @@
 <?php include 'sidebar.php' ?>
 
-    <!-- CSS style-->
-    <style>
-        .maincontent {
-            display: flex;
-            flex-direction: column;
-            padding: 20px;
-        }
-        .box-container {
-            box-shadow: 0 0 15px 2px whitesmoke;
-            backdrop-filter: blur(12px) saturate(180%);
-            -webkit-backdrop-filter: blur(12px) saturate(180%);
-            background-color: rgba(255, 255, 255, 0.44);
-            border-radius: 12px;
-            border: 1px solid rgba(209, 213, 219, 0.3);
-            padding: 20px;
-        }
-        .grid-container {
-            display: grid;
-            grid-template-columns: 1fr 1fr;
-            gap: 20px;
-        }
-        .lorem-box-span {
-            grid-column: 1 / span 2;
-        }
-    </style>
+    <link rel="stylesheet" href="report.css">
 
     <div class="maincontent">
         <h3>Reports</h3>
@@ -34,19 +10,77 @@
                         
                     </header>
 
-                    <div class="box-container grid-item lorem-box-span">
+                    <div class="box-container grid-item lorem-box-span item-1">
                         <section class="overview">
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, eius ut at ratione aperiam qui accusamus sapiente magnam, rerum totam sunt quis non, natus tempora vel itaque voluptates nam reprehenderit.</p>
                         </section>
                     </div>
-                     
+  
+                    <div class="box-container grid-item item-2">
+                        </section class="low-stock-performance">
+                                <canvas id="myBarChart" width="400" height="200"></canvas>
+                                <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+                                <!--Bar Chart -->
+                                <script>
+                                    const label = ['PC', 'Keyboard', 'Mouse', 'Speaker', 'Headphone', 'HDMI', 'VGA'];
+                                    const details = {
+                                    labels: label,
+                                    datasets: [{
+                                        label: 'My First Dataset',
+                                        data: [65, 59, 80, 81, 56, 55, 40],
+                                        backgroundColor: [
+                                        'rgba(255, 99, 132, 0.2)',
+                                        'rgba(255, 159, 64, 0.2)',
+                                        'rgba(255, 205, 86, 0.2)',
+                                        'rgba(75, 192, 192, 0.2)',
+                                        'rgba(54, 162, 235, 0.2)',
+                                        'rgba(153, 102, 255, 0.2)',
+                                        'rgba(201, 203, 207, 0.2)'
+                                        ],
+                                        borderColor: [
+                                        'rgb(255, 99, 132)',
+                                        'rgb(255, 159, 64)',
+                                        'rgb(255, 205, 86)',
+                                        'rgb(75, 192, 192)',
+                                        'rgb(54, 162, 235)',
+                                        'rgb(153, 102, 255)',
+                                        'rgb(201, 203, 207)'
+                                        ],
+                                        borderWidth: 1
+                                    }]
+                                    };
+                                    
+                                    const form = {
+                                    type: 'bar',
+                                    data: details,
+                                    options: {
+                                        scales: {
+                                        y: {
+                                            beginAtZero: true
+                                        }
+                                        }
+                                    },
+                                    };
+
+                                    const cty = document.getElementById('myBarChart').getContext('2d');
+                                    const myBarChart = new Chart(cty, form);
+                                </script>  
+                        </section>
+                    </div>
+
+                    <div class="box-container grid-item item-3">
+                        <section class="list">
+
+                        </section>
+                    </div>
+                    
                     <div class="box-container grid-item">
-                        <section class="monthly-performance">
+                        <section class="monthly-performance item-4">
                             <canvas id="myLineChart" width="400" height="200"></canvas>
                             <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
                             <!--Line Chart -->
                             <script>
-                                const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+                                const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
                                 const data = {
                                 labels: labels,
                                 datasets: [{
@@ -66,16 +100,10 @@
                                 const ctx = document.getElementById('myLineChart').getContext('2d');
                                 const myLineChart = new Chart(ctx, config);
                             </script>
-                        </div>
-            
-                    <div class="box-container grid-item">
                         </section>
-
-                        <section>    
                     </div>
                     
-                    
-                </section>
+                </div>
             </div>                    
     </div>
 
