8725e8a0160bb5c4b1cc92d397939d904b567d40
oone <bariquitsheilaaa@gmail.com>
2025-10-28 10:21:04 +0800

    update

--- sidebar.php
+++ sidebar.php
@@ -7,22 +7,6 @@
 
 <div class="sidebar">
 
-    <!-- Toggle Icon -->
-      <!-- <div class="collapse" id="navbarToggleExternalContent" data-bs-theme="dark">
-      <div class="bg-dark p-4">
-        <h5 class="text-body-emphasis h4">Collapsed content</h5>
-        <span class="text-body-secondary">Toggleable via the navbar brand.</span>
-      </div>
-    </div>
-    <nav class="navbar navbar-dark bg-dark">
-      <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      </div>
-    </nav> -->
-    
-
     <div class="ucp-logo">
         <img src="asset/img/UC.png" style="width: 50px; height: 50px;">
         <h5>Universal College Inventory</h5>
